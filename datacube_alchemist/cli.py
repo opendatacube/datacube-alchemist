a0af8970c2c6deb32d463ca3057b215829013377
Duncan Gray <duncan.gray@ga.gov.au>
2019-08-16 17:28:46 +1000

    minor clean-up and addingtimeout

--- datacube_alchemist/cli.py
+++ datacube_alchemist/cli.py
@@ -82,38 +82,36 @@ def add_to_queue(config_file, message_queue, expressions, environment=None, limi
     tasks = alchemist.generate_tasks(expressions, limit=limit)
     for task in tasks:
         pickled_task = cloudpickle.dumps(task)
-        atts ={
-        'pickled_task': {
-            'BinaryValue': pickled_task,
-            'DataType': 'Binary'
-                        }
-                }
-
+        atts ={'pickled_task': {'BinaryValue': pickled_task, 'DataType': 'Binary'}}
+        # The information is in the pickled_task message attribute
+        # The message body is not used by the s/w
         queue.send_message(MessageBody=task.dataset.local_uri,  MessageAttributes=atts)
 
 
 @cli.command()
 @click.argument('message_queue')
-
-def pull_from_queue(message_queue):
+@click.option('--sqs_timeout', '-s', type=int,
+              help='The SQS message Visability Timeout.',
+              default=1000)
+def pull_from_queue(message_queue, sqs_timeout=None):
     # Set up the queue
     sqs = boto3.resource('sqs')
     queue = sqs.get_queue_by_name(QueueName=message_queue)
 
     messages = queue.receive_messages(
-        VisibilityTimeout=10,
-#        VisibilityTimeout=VISIBILITYTIMEOUT,
+        VisibilityTimeout=sqs_timeout,
         MaxNumberOfMessages=1,
     MessageAttributeNames=['All']
     )
-    _LOG.info("Found task to process: {}".format(messages))
-    message = None
     if len(messages) > 0:
         message = messages[0]
         pickled_task = message.message_attributes['pickled_task']['BinaryValue']
         task = cloudpickle.loads(pickled_task)
         _LOG.info("Found task to process: {}".format(task))
         execute_task(task)
+
+        message.delete()
+        _LOG.info("SQS message deleted")
     else:
         _LOG.warning("No messages!")
 
