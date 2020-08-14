4010d937e6e939f8213a7750dda0889e17b52443
nehem <nehemiah@gmail.com>
2020-08-14 11:11:15 +1000

    Sample kube deployment yaml, few debug timer

--- datacube_alchemist/cli.py
+++ datacube_alchemist/cli.py
@@ -8,26 +8,21 @@ import boto3
 import click
 import cloudpickle
 import structlog
-
 from datacube import Datacube
 from datacube.ui import click as ui
+
 from datacube_alchemist._dask import setup_dask_client
-from datacube_alchemist.worker import Alchemist, execute_with_dask, \
-    execute_task, execute_pickled_task
+from datacube_alchemist.worker import Alchemist, execute_with_dask, execute_task, execute_pickled_task
 
 _LOG = structlog.get_logger()
 
 # Define common options for all the commands
-message_queue_option = click.option('--message_queue', '-M',
-                                    help='Name of an AWS SQS Message Queue')
+message_queue_option = click.option("--message_queue", "-M", help="Name of an AWS SQS Message Queue")
 
-environment_option = click.option('--environment', '-E',
-                                  help='Name of the Datacube environment to connect to.')
-
+environment_option = click.option("--environment", "-E", help="Name of the Datacube environment to connect to.")
 
 def cli_with_envvar_handling():
-    cli(auto_envvar_prefix='ALCHEMIST')
-
+    cli(auto_envvar_prefix="ALCHEMIST")
 
 @click.group(context_settings=dict(max_content_width=120))
 def cli():
@@ -35,13 +30,10 @@ def cli():
     Transform Open Data Cube Datasets into a new type of Dataset
     """
 
-
 @cli.command()
-@click.option('--environment', '-E',
-              help='Name of the datacube environment to connect to.')
-@click.option('--limit', type=int,
-              help='For testing, specify a small number of tasks to run.')
-@click.argument('config_file')
+@click.option("--environment", "-E", help="Name of the datacube environment to connect to.")
+@click.option("--limit", type=int, help="For testing, specify a small number of tasks to run.")
+@click.argument("config_file")
 @ui.parsed_search_expressions
 def run_many(config_file, expressions, environment=None, limit=None):
     """
@@ -55,11 +47,10 @@ def run_many(config_file, expressions, environment=None, limit=None):
     client = setup_dask_client(alchemist.config)
     execute_with_dask(client, tasks)
 
-
 @cli.command()
 @environment_option
-@click.argument('config_file')
-@click.argument('input_dataset')
+@click.argument("config_file")
+@click.argument("input_dataset")
 def run_one(config_file, input_dataset, environment=None):
     """
     Run with CONFIG_FILE on a single INPUT_DATASET
@@ -72,11 +63,9 @@ def run_one(config_file, input_dataset, environment=None):
     try:
         ds = dc.index.datasets.get(input_dataset)
     except ValueError as e:
-        _LOG.info("Couldn't find dataset with ID={} with exception {} trying by URL".format(
-            input_dataset, e
-        ))
+        _LOG.info("Couldn't find dataset with ID={} with exception {} trying by URL".format(input_dataset, e))
         # Couldn't find a dataset by ID, try something
-        if '://' in input_dataset:
+        if "://" in input_dataset:
             # Smells like a url
             input_url = input_dataset
         else:
@@ -89,13 +78,11 @@ def run_one(config_file, input_dataset, environment=None):
     task = alchemist.generate_task(ds)
     execute_task(task)
 
-
 @cli.command()
 @environment_option
-@click.option('--limit', type=int,
-              help='For testing, limit the number of tasks to create.')
+@click.option("--limit", type=int, help="For testing, limit the number of tasks to create.")
 @message_queue_option
-@click.option('--config_file')
+@click.option("--config_file")
 @ui.parsed_search_expressions
 def addtoqueue(config_file, message_queue, expressions, environment=None, limit=None):
     """
@@ -109,7 +96,7 @@ def addtoqueue(config_file, message_queue, expressions, environment=None, limit=
     _LOG.info("Start add to queue.")
     start_time = time.time()
     # Set up the queue
-    sqs = boto3.resource('sqs')
+    sqs = boto3.resource("sqs")
     queue = sqs.get_queue_by_name(QueueName=message_queue)
 
     # Load Configuration file
@@ -122,12 +109,12 @@ def addtoqueue(config_file, message_queue, expressions, environment=None, limit=
         pickled_task = cloudpickle.dumps(task)
         msize = sys.getsizeof(pickled_task)
         sum_size += msize
-        atts = {'pickled_task': {'BinaryValue': pickled_task, 'DataType': 'Binary'}}
+        atts = {"pickled_task": {"BinaryValue": pickled_task, "DataType": "Binary"}}
 
         # The information is in the pickled_task message attribute
         # The message body is not used by the s/w
-        body = task.dataset.uris[0] if task.dataset.uris is not None else 'location not known.'
-        messages.append({'MessageBody': body, 'Id': str(count), 'MessageAttributes': atts})
+        body = task.dataset.uris[0] if task.dataset.uris is not None else "location not known."
+        messages.append({"MessageBody": body, "Id": str(count), "MessageAttributes": atts})
 
         # Call can't exceed 262,144 bytes.  I'm only measuring the config file size though.
         # And I'm adding messages until it's over a limit.
@@ -142,29 +129,22 @@ def addtoqueue(config_file, message_queue, expressions, environment=None, limit=
         _ = _push_messages(queue, messages)
     _LOG.info("Ending. Pushed {} items in {:.2f}s.".format(count + 1, time.time() - start_time))
 
-
 @cli.command()
 @message_queue_option
-@click.option('--sqs_timeout', '-S', type=int,
-              help='The SQS message Visability Timeout.',
-              default=400)
+@click.option("--sqs_timeout", "-S", type=int, help="The SQS message Visability Timeout.", default=400)
 def pullfromqueue(message_queue, sqs_timeout=None):
     """
     Process a single task from an AWS SQS Queue
     """
     _LOG.info("Start pull from queue.")
     # Set up the queue
-    sqs = boto3.resource('sqs')
+    sqs = boto3.resource("sqs")
     queue = sqs.get_queue_by_name(QueueName=message_queue)
 
-    messages = queue.receive_messages(
-        VisibilityTimeout=sqs_timeout,
-        MaxNumberOfMessages=1,
-        MessageAttributeNames=['All']
-    )
+    messages = queue.receive_messages(VisibilityTimeout=sqs_timeout, MaxNumberOfMessages=1, MessageAttributeNames=["All"])
     if len(messages) > 0:
         message = messages[0]
-        pickled_task = message.message_attributes['pickled_task']['BinaryValue']
+        pickled_task = message.message_attributes["pickled_task"]["BinaryValue"]
         dataset_id, metadata_path = execute_pickled_task(pickled_task)
 
         # TODO: see if we can catch failed tasts that return and don't delete the message if they failed
@@ -173,43 +153,38 @@ def pullfromqueue(message_queue, sqs_timeout=None):
     else:
         _LOG.warning("No messages!")
 
-
 # TODO: don't repeat contents of this function in the above function
 @cli.command()
-@click.option('--message_queue', '-M')
-@click.option('--sqs_timeout', '-S', type=int,
-              help='The SQS message Visibility Timeout.',
-              default=400)
+@click.option("--message_queue", "-M")
+@click.option("--sqs_timeout", "-S", type=int, help="The SQS message Visibility Timeout.", default=400)
 def processqueue(message_queue, sqs_timeout=None):
     """
     Process all available tasks from an AWS SQS Queue
     """
     _LOG.info("Start pull from queue.")
     # Set up the queue
-    sqs = boto3.resource('sqs')
+    sqs = boto3.resource("sqs")
     queue = sqs.get_queue_by_name(QueueName=message_queue)
 
-    more_mesages = True
-    while more_mesages:
-        messages = queue.receive_messages(
-            VisibilityTimeout=int(sqs_timeout),
-            MaxNumberOfMessages=1,
-            MessageAttributeNames=['All']
-        )
-        if len(messages) == 0:
-            # No messages, exit successfully
-            _LOG.info("No new messages, exiting successfully")
-            more_mesages = False
-        else:
-            message = messages[0]
-            pickled_task = message.message_attributes['pickled_task']['BinaryValue']
-            dataset_id, metadata_path = execute_pickled_task(pickled_task)
+    # more_mesages = True
+    # while more_mesages:
+    #     time.sleep(1)  # Todo remove once debugged
+    messages = queue.receive_messages(VisibilityTimeout=int(sqs_timeout), MaxNumberOfMessages=1, MessageAttributeNames=["All"])
+    if len(messages) == 0:
+        # No messages, exit successfully
+        _LOG.info("No new messages, exiting successfully")
+        more_mesages = False
+    else:
+        message = messages[0]
+        _LOG.info(f"Message received: {message}")
 
-            # TODO: see if we can catch failed tasts that return and don't delete the message if they failed
+        pickled_task = message.message_attributes["pickled_task"]["BinaryValue"]
+        dataset_id, metadata_path = execute_pickled_task(pickled_task)
 
-            message.delete()
-            _LOG.info("SQS message deleted")
+        # TODO: see if we can catch failed tasts that return and don't delete the message if they failed
 
+        message.delete()
+        _LOG.info("SQS message deleted")
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     cli_with_envvar_handling()
